b420d7eedcab2c92845369fe1a6c25eedd797333
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-08-21 21:00:44 +0545

    responsive 404

--- pages/404/index.js
+++ pages/404/index.js
@@ -5,6 +5,8 @@ import NextLayout from '../../components/layout'
 const DynamicHead = dynamic(() => import('../../components/head'))
 
 import notFound from '../../public/assets/MessyDoodle.svg'
+import homeIcon from '../../public/assets/icons/home-2.svg'
+
 
 
 const index = () => {
@@ -12,12 +14,12 @@ const index = () => {
         <NextLayout>
             <DynamicHead />
             <div className='h-screen flex justify-center items-center flex-col'>
-                <img src={notFound} alt="Not Found" className='w-1/4' />
-                <div className="w-1/3 h-auto mx-auto flex p-6 items-center bg-bgDark rounded-lg shadow-md font-body mt-16 text-white hover:shadow-xl justify-between">
+                <img src={notFound} alt="Not Found" className='w-1/4 xl:w-1/2 sm:w-3/4'/>
+                <div className="w-1/2 xl:w-3/4 h-auto mx-auto flex p-6 items-center bg-bgDark rounded-lg shadow-md font-body mt-16 text-white hover:shadow-xl justify-between flex-wrap">
                     <span>Sorry! The page you are looking for is unavailabe.😥</span>
                     <Link href='/'>
-                        <button class="transition duration-500 ease-in-out font-semibold bg-white text-bgDark py-2 px-4 border border-white rounded transform hover:scale-105">
-                            Home
+                        <button class="sm:mt-3 transition duration-500 ease-in-out font-semibold bg-white text-bgDark py-1 px-2 border border-white rounded transform hover:scale-105">
+                            <img src={homeIcon} alt="Home"/>
                         </button>
                     </Link>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   important: true,
   theme: {
+    screens: {
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
     fontFamily: {
       display: ['Roboto', 'sans-serif'],
       body: ['Roboto', 'sans-serif'],
