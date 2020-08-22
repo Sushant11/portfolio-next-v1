398b3a269a7d5379bdd273a231e367328f2e67a0
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-08-22 16:08:48 +0545

    tap

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NextLayout = ({children}) => {
     return (
-        <div className='antialiased w-7/12 lg:w-11/12 md:w-auto mx-auto px-2'>
+        <div className='antialiased w-7/12 lg:w-11/12 md:w-auto mx-auto px-2 tap-highlight'>
             {children}
         </div>
     );
