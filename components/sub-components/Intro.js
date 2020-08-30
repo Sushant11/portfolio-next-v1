8ba8d97ebc0b4858c055e9ee94d69711881006e1
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-08-30 17:39:23 +0545

    btnHover

--- components/sub-components/Intro.js
+++ components/sub-components/Intro.js
@@ -27,7 +27,7 @@ const Intro = () => {
                         </div>
                     </div>
                     <div>
-                        <button className="bg-white font border py-4 px-6 rounded-sm shadow-xl my-12 md:my-2 focus:outline-none hover:text-gray-700 font- inline-flex items-center hover:shadow-sm border-gray-700 border-opacity-50 hover:border-gray-400 transition duration-300 ease-in-out tracking-wide">
+                        <button className="bg-white font border py-4 px-6 rounded-sm shadow-sm my-12 md:my-2 focus:outline-none hover:text-gray-700 font- inline-flex items-center hover:shadow-lg hover:-translate-y-1 border-darkGrey border-opacity-25 hover:border-opacity-0 transition duration-300 ease-in-out transform tracking-wide">
                             <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="paper-airplane w-6 h-6 transform rotate-45 mr-2"><path strokeinecap="round" strokeLinejoin="round" strokeWidth="1" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg>
                     Get In Touch
                     </button>
