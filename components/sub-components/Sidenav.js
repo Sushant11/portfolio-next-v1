911cdda82137e3f0148283c7a951a9be19b5f3e7
Sushant Chitrakar <sushantchitrakar@gmail.com>
2020-11-27 22:44:04 +0545

    Resume

--- components/sub-components/Sidenav.js
+++ components/sub-components/Sidenav.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Link, animateScroll as scroll } from "react-scroll";
 
-const Sidenav = ({ isOpen, setOpen, handleNav }) => {
+const Sidenav = ({ isOpen, setOpen }) => {
     return (
-        <div className={`hidden items-center md:block pl-6 font-normal z-20 text-gray-800 fixed h-full bg-white right-0 shadow-xl pt-12 transition duration-500 ease-in w-1/2`}>
-            <a type='button' onClick={handleNav} className='cursor-pointer mb-12'>
-                <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="x-circle sm:mr-4 nav-icons "><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            </a>
+        <div className={`hidden items-center md:block pl-6 font-normal z-20 text-gray-800 absolute h-full bg-white right-0 shadow-xl pt-24 transition duration-500 ease-in w-1/2`}>
             <div className='flex flex-col'>
                 <div className='hover:text-gray-600 my-4'>
                     <Link
@@ -49,7 +46,7 @@ const Sidenav = ({ isOpen, setOpen, handleNav }) => {
                     </Link>
                 </div>
                 <div className='hover:text-gray-600 my-4'>
-                    <a type='button' href='#' onClick={() => setOpen(!isOpen)} className='inline-flex items-center'>
+                    <a type='button' href="/assets/Resume.pdf" onClick={() => setOpen(!isOpen)} className='inline-flex items-center'>
                         <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="document-text nav-icons"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
                         <span className='mx-4 text-xl'>Resume</span>
                     </a>
